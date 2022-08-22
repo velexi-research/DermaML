7bcf895e38c11a0213357685e70f2214e6b303a3
Kevin Chu <kevin@velexi.com>
2022-08-22 13:35:54 -0700

    Improve logic for specifying image types in prepare-data.py

--- bin/prepare-data.py
+++ bin/prepare-data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Script for preparing raw image data for feature extraction and engineering.
+Script for preparing raw image data for feature extraction.
 """
 
 # --- Imports
@@ -9,6 +9,7 @@ Script for preparing raw image data for feature extraction and engineering.
 import glob
 import os
 from pathlib import Path
+from typing import Optional
 
 # External packages
 import skimage
@@ -21,37 +22,47 @@ import dermaml.data
 
 # --- Main program
 
-def main(src_dir: Path, dst_dir: Path, image_type: str = "jpeg") -> None:
+def main(src_dir: Path,
+         dst_dir: Path,
+         image_type: Optional[str] = "all") -> None:
     """
-    Prepare raw image data for feature extraction and engineering.
+    Prepare raw image data for feature extraction.
     """
+    # --- Check arguments
+
+    if image_type is None or image_type.lower() == "all":
+        image_ext_list = ["gif", "jpeg", "jpg", "png", "tiff"]
+    else:
+        if image_type.lower() in ["jpeg", "jpg"]:
+            image_ext_list = ["jpeg", "jpg"]
+        else:
+            image_ext_list = [image_type]
+
     # --- Preparations
 
     # Prepare destination directory
     os.makedirs(dst_dir, exist_ok=True)
 
     # Get list of image files
-    image_files = []
-    if image_type in ["jpeg", "jpg"]:
-        image_files.extend(glob.glob(os.path.join(src_dir, '*.jpeg')))
-        image_files.extend(glob.glob(os.path.join(src_dir, '*.jpg')))
-    else:
-        image_files.extend(glob.glob(os.path.join(src_dir, f'*.{image_type}')))
+    image_paths = []
+    for image_ext in image_ext_list:
+        image_paths.extend(glob.glob(os.path.join(src_dir, f'*.{image_ext}')))
 
-    # --- Pre-process image files
+    # --- Prepare image files for feature extraction.
 
-    for image_file in image_files:
+    for image_path in image_paths:
         # Load image
-        image = skimage.io.imread(image_file)
+        image = skimage.io.imread(image_path)
 
         # Remove background
         if len(image.shape) > 2:
             image = dermaml.data.remove_background(image)
 
         # Save image
-        filename = os.path.basename(image_file)
-        output_path = f"{os.path.splitext(filename)[0]}.png"
-        image = skimage.io.imsave(os.path.join(dst_dir, output_path), image)
+        filename = os.path.basename(image_path)
+        output_path = os.path.join(dst_dir,
+                                   f"{os.path.splitext(filename)[0]}.png")
+        skimage.io.imsave(output_path, image)
 
 
 # --- Run app

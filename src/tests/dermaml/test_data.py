1373c803e9042384fab1c76374f1eb78dd820423
Kevin Chu <kevin@velexi.com>
2024-06-13 08:56:51 -0700

    Add placeholders for unit tests.

---
+++ src/tests/dermaml/test_data.py
@@ -0,0 +1,26 @@
+"""
+Unit tests for the `dermaml.data` module
+"""
+
+# --- Imports
+
+# Standard library
+
+# Local imports
+import dermaml.data
+
+
+# --- Tests
+
+
+def test_generate_synthetic_dataset():
+    """
+    Test generate_synthetic_dataset().
+    """
+    # --- Preparations
+
+    # TODO
+
+    # --- Test
+
+    # TODO
--- src/tests/dermaml/test_image.py
+++ src/tests/dermaml/test_image.py
@@ -39,3 +39,66 @@ def test_remove_alpha_channel():
 
     assert isinstance(processed_image, np.ndarray)
     assert processed_image.shape[-1] == 3
+
+
+def test_remove_background():
+    """
+    Test remove_background().
+    """
+    # --- Preparations
+
+    # Image filename
+    # TODO: update file
+    image_filename = "remove-alpha-channel-test-image.png"
+
+    # Create path to image file
+    image_path = os.path.join(os.path.dirname(__file__), "data", image_filename)
+
+    # Open image and convert it to a NumPy array
+    image = np.asarray(Image.open(image_path))
+
+    # --- Test
+
+    processed_image = dermaml.image.remove_background(image)
+
+    # TODO
+
+
+def test_crop_palm():
+    """
+    Test crop_palm().
+    """
+    # --- Preparations
+
+    # Image filename
+    # TODO: update file
+    image_filename = "remove-alpha-channel-test-image.png"
+
+    # Create path to image file
+    image_path = os.path.join(os.path.dirname(__file__), "data", image_filename)
+
+    # --- Test
+
+    processed_image = dermaml.image.crop_palm(image_path)
+
+    # TODO
+
+
+def test_multi_crop_palm():
+    """
+    Test multi_crop_palm().
+    """
+    # --- Preparations
+
+    # Image filename
+    # TODO: update file
+    image_filename = "remove-alpha-channel-test-image.png"
+
+    # Create path to image file
+    image_path = os.path.join(os.path.dirname(__file__), "data", image_filename)
+
+    # --- Test
+
+    processed_image = dermaml.image.multi_crop_palm(image_path)
+
+    # TODO

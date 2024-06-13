ace36dcc5f57628ccc4e38687e3da28a43d18875
Kevin Chu <kevin@velexi.com>
2024-06-12 17:52:23 -0700

    Fix black errors.

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,20 +1,5 @@
-#
-#   Copyright 2022 Kevin Chu
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
 """
-Unit tests for `utils.py` module.
+Unit tests for the `utils` module
 """
 
 # --- Imports
@@ -27,6 +12,7 @@ import utils
 
 
 # --- Tests
+
 
 def test_get_experiment_name():
     """
@@ -48,9 +34,8 @@ def test_get_experiment_name():
     # include_timestamp = True
     description = "Experiment Description"
     description_slugified = "experiment_description"
-    experiment_name = utils.get_experiment_name(description,
-                                                include_timestamp=True)
-    experiment_name_parts = experiment_name.split('-')
+    experiment_name = utils.get_experiment_name(description, include_timestamp=True)
+    experiment_name_parts = experiment_name.split("-")
     assert len(experiment_name_parts) == 5
-    assert '-'.join(experiment_name_parts[0:3]) == expected_date
+    assert "-".join(experiment_name_parts[0:3]) == expected_date
     assert experiment_name_parts[-1] == description_slugified

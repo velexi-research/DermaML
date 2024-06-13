bdcc1365d4aee94cf75ecce71f1d9214373d56fd
Kevin Chu <kevin@velexi.com>
2024-06-12 17:49:23 -0700

    Add unit test fo remove_alpha_channel().

---
+++ src/tests/dermaml/test_image.py
@@ -0,0 +1,41 @@
+"""
+Unit tests for the `dermaml.image` module
+"""
+
+# --- Imports
+
+# Standard library
+import os
+
+# Local imports
+import dermaml.image
+from PIL import Image
+import numpy as np
+
+
+# --- Tests
+
+
+def test_remove_alpha_channel():
+    """
+    Test remove_alpha_channel().
+    """
+    # --- Preparations
+
+    # Image filename
+    image_filename = "remove-alpha-channel-test-image.png"
+
+    # Create path to image file
+    image_path = os.path.join(os.path.dirname(__file__), "data", image_filename)
+
+    # Open image and convert it to a NumPy array
+    image = Image.open(image_path)
+    image_np = np.asarray(image)
+    assert image_np.shape[-1] == 4
+
+    # --- Test
+
+    processed_image = dermaml.image.remove_alpha_channel(image_np)
+
+    assert isinstance(processed_image, np.ndarray)
+    assert processed_image.shape[-1] == 3
